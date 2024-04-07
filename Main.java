2ba0c1343042c199147bb1b19459d4bb96edeccf
Karol Olbryś  <karlson2002@gmail.com>
2024-04-07 18:58:49 +0200

    Utworzenie grupy

--- Main.java
+++ Main.java
@@ -1,8 +1,18 @@
+import java.util.ArrayList;
+
 public class Main {
    public static void main(String[] args) {
         Student student1 = new Student("Karol", "Kowalski", 1, "kakowalski@pjwstk.edu.pl", "Złota 44");
         student1.grades = new double[]{3,4,5,2,3,4,4,5};
         
         System.out.println("Średnia ocena studenta to " + student1.studentAvgGrade());
+
+        StudentGroup group1 = new StudentGroup("12c", new ArrayList<Student>());
+
+        System.out.println(group1.showStudentsAmount());
+
+        group1.addStudentToGroup(student1);
+
+        System.out.println(group1.showStudentsAmount());
    }
 }
--- StudentGroup.java
+++ StudentGroup.java
@@ -1,13 +1,33 @@
+import java.util.ArrayList;
+
 public class StudentGroup {
     public String nazwa;
-    public Student[] student;
+    public ArrayList<Student> student;
 
-    public StudentGroup(String name){
+    public StudentGroup(String name, ArrayList<Student> st1){
         this.nazwa = name;
+        this.student = st1;
     }
 
-    public void addStudentToGroup(Student student){
-        
+    public void addStudentToGroup(Student st){
+        if(this.student.size()>=15){
+            throw new IllegalArgumentException("Za duzo studentow w grupie");
+        }else{
+            for(int i = 0; i < student.size(); i++){
+                if (st.equals(student.get(i))) {
+                    throw new IllegalArgumentException("Jest juz taki student w tej grupie");
+                }
+        this.student.add(st);
+            }
+        }
+    }
+
+    public String showStudentInGroup(){
+        return this.student.toString();
+    }
+
+    public int showStudentsAmount(){
+        return this.student.size();
     }
 
 
